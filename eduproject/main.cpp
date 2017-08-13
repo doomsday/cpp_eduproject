f283618df1e002d0c4e4cca9e8f82b6942719bd1
Mikhail Gashenko <dr.psycho@live.com>
2017-08-13 16:46:46 +0600

    Binding an acceptor socket to an endpoint

--- eduproject/main.cpp
+++ eduproject/main.cpp
@@ -5,46 +5,32 @@ using namespace boost;
 
 int main() {
 
-  /* Resolving a DNS name */
+  /* Binding an acceptor socket to an endpoint */
 
-  // Step 1. Assume that the client application has already obtained the DNS name and protocol port number and
-  // represented them as strings.
-  std::string host = "google.com";
-  std::string port_num = "80";
+  // Step 1. Here we assume that the server application has already obtained the protocol port number.
+  unsigned short port_num = 3333;
 
-  // Step 2. Used by the resolver to access underlying OS's services during a DNS name resolution process.
+  // Step 2. Creating an endpoint representing all IP addresses available on the host.
+  asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(), port_num);
+
+  // Used by 'acceptor' class constructor.
   asio::io_service ios;
 
-  // Step 3. Creating a query. Because the service is specified as a protocol port number (in our case, 3333) and not
-  // as a service name (for example, HTTP, FTP, and so on), we passed the
-  // asio::ip::tcp::resolver::query::numeric_service ﬂag to explicitly inform the query object about that, so that it
-  // properly parses the port number value.
-  asio::ip::tcp::resolver::query resolver_query(host, port_num, asio::ip::tcp::resolver::query::numeric_service);
+  // Step 3. Creating and opening an acceptor socket.
+  asio::ip::tcp::acceptor acceptor(ios, ep.protocol());
 
-  // Step 4. Creating a resolver.
-  asio::ip::tcp::resolver resolver(ios);
-
-  // Used to store information about error that happens during the resolution process.
   boost::system::error_code ec;
 
-  // Step 5. The DNS name resolution is performed.
-  asio::ip::tcp::resolver::iterator it = resolver.resolve(resolver_query, ec);
+  // Step 4. Binding the acceptor socket.
+  acceptor.bind(ep, ec);
 
   // Handling errors if any.
   if (ec != 0) {
-  // Failed to resolve the DNS name. Breaking execution.
-    std::cout << "Failed to resolve a DNS name."
-              << "Error code = " << ec.value()
-              << ". Message = " << ec.message();
+    // Failed to bind the acceptor socket. Breaking execution.
+    std::cout << "Failed to bind the acceptor socket."
+              << "Error code = " << ec.value() << ". Message: "
+              << ec.message();
     return ec.value();
-  }
-
-  asio::ip::tcp::resolver::iterator it_end;
-
-  for(; it != it_end; ++it) {
-    // Here we can access the endpoint like this.
-    asio::ip::tcp::endpoint ep = it->endpoint();
-    std::cout << ep.address() << std::endl;
   }
 
   return EXIT_SUCCESS;
