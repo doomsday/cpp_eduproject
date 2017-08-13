04ef5f484a6107bfa2c0200370ee5e35d90b8cf7
Mikhail Gashenko <dr.psycho@live.com>
2017-08-13 17:57:23 +0600

    Connecting a socket

--- eduproject/main.cpp
+++ eduproject/main.cpp
@@ -5,32 +5,38 @@ using namespace boost;
 
 int main() {
 
-  /* Binding an acceptor socket to an endpoint */
+  /* Connecting a socket */
 
-  // Step 1. Here we assume that the server application has already obtained the protocol port number.
-  unsigned short port_num = 3333;
+  // Step 1. Assume that the client application has already // obtained the IP address and protocol port number of the
+  // target server.
+  std::string raw_ip_address = "127.0.0.1";
+  unsigned short port_num = 27017;
 
-  // Step 2. Creating an endpoint representing all IP addresses available on the host.
-  asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(), port_num);
+  try {
+    // Step 2. Creating an endpoint designating a target server application.
+    asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address), port_num);
 
-  // Used by 'acceptor' class constructor.
-  asio::io_service ios;
+    asio::io_service ios;
 
-  // Step 3. Creating and opening an acceptor socket.
-  asio::ip::tcp::acceptor acceptor(ios, ep.protocol());
+    // Step 3. Creating and opening a socket. This function connects the socket to the server. The connection is
+    // performed synchronously. Before performing the connection establishment procedure, the socket's connect() method
+    // will bind the socket to the endpoint consisting of an IP address and a protocol port number chosen by the
+    // operating system
+    asio::ip::tcp::socket sock(ios, ep.protocol());
 
-  boost::system::error_code ec;
+    // Step 4. Connecting a socket.
+    sock.connect(ep);
 
-  // Step 4. Binding the acceptor socket.
-  acceptor.bind(ep, ec);
-
-  // Handling errors if any.
-  if (ec != 0) {
-    // Failed to bind the acceptor socket. Breaking execution.
-    std::cout << "Failed to bind the acceptor socket."
-              << "Error code = " << ec.value() << ". Message: "
-              << ec.message();
-    return ec.value();
+    // At this point socket 'sock' is connected to the server application and can be used to send data to or receive
+    // data from it.
+  }
+    // Overloads of asio::ip::address::from_string() and asio::ip::tcp::socket::connect() used here throw exceptions in
+    // case of error condition. Both methods have overloads that don't throw exceptions and accept an object of
+    // the boost::system::error_code class.
+    catch (system::system_error &e) {
+      std::cout << "Error occurred! Error code = " << e.code()
+                << ". Message: " << e.what();
+    return e.code().value();
   }
 
   return EXIT_SUCCESS;
