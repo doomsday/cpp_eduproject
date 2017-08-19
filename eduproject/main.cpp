80acebf7e6209b9fef7ad81897c879085cce561c
Mikhail Gashenko <dr.psycho@live.com>
2017-08-19 19:33:24 +0600

    Reading from a TCP socket synchronously  (simplified using asio::read)

--- eduproject/main.cpp
+++ eduproject/main.cpp
@@ -7,7 +7,7 @@ std::string readFromSocket(asio::ip::tcp::socket &sock);
 
 int main() {
 
-  /* Reading from a TCP socket synchronously */
+  /* Reading from a TCP socket synchronously  (simplified using asio::read) */
 
   std::string raw_ip_address = "127.0.0.1";
   unsigned short port_num = 3333;
@@ -34,14 +34,8 @@ int main() {
 std::string readFromSocket(asio::ip::tcp::socket &sock) {
   const unsigned char MESSAGE_SIZE = 7;
   char buf[MESSAGE_SIZE];
-  std::size_t total_bytes_read = 0;
 
-  while (total_bytes_read != MESSAGE_SIZE) {
-    total_bytes_read += sock.read_some(
-        asio::buffer(buf + total_bytes_read,
-                     MESSAGE_SIZE - total_bytes_read)
-    );
-  }
+  asio::read(sock, asio::buffer(buf, MESSAGE_SIZE));
 
-  return std::string(buf, total_bytes_read);
+  return std::string(buf, MESSAGE_SIZE);
 }
