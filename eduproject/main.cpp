91f6121873b07167da397fdc6fb733456559b625
Mikhail Gashenko <dr.psycho@live.com>
2017-08-27 09:34:39 +0600

    Shutting down and closing a socket

--- eduproject/main.cpp
+++ eduproject/main.cpp
@@ -1,83 +1,35 @@
-#include <boost/predef.h>
-
-// Step 1. If the application is intended to run on Windows XP or Windows Server 2003, define flags that enable
-// asynchronous operation canceling on these versions of Windows.
-#ifdef BOOST_OS_WINDOWS
-#if _WIN32_WINNT <= 0x0502 // Windows Server 2003 or earlier.
-#define BOOST_ASIO_DISABLE_IOCP
-#define BOOST_ASIO_ENABLE_CANCELIO
-#endif
-#endif
-
 #include <boost/asio.hpp>
 #include <iostream>
 #include <thread>
 
+#include "server.hpp"
+
 using namespace boost;
+
+void communicate(asio::ip::tcp::socket& sock);
+
+std::thread start_server();
+
+void stop_server(std::thread& server);
 
 int main() {
 
-  /* Canceling asynchronous operations */
+  /* Shutting down and closing a socket */
 
+  // Starting server.
+  std::thread server = start_server();
+
+  // Starting client.
   std::string raw_ip_address = "127.0.0.1";
   unsigned short port_num = 3333;
 
   try {
-    // Step 2. Allocate and open a TCP or UDP socket. It may be an active or passive (acceptor) socket in the client or
-    // server application
     asio::ip::tcp::endpoint ep(asio::ip::address::from_string(raw_ip_address), port_num);
     asio::io_service ios;
-    auto sock(std::make_shared<asio::ip::tcp::socket>(ios, ep.protocol()));
-
-
-    // Step 3. Define a callback function or functor for an asynchronous operation. If needed, in this callback,
-    // implement a branch of code that handles the situation when the operation has been canceled.
-    // Step 4. Initiate one or more asynchronous operations and specify a function or an object defined in step 4 as a
-    // callback.
-
-    // Asynchronously connect a socket to the specified remote endpoint.
-    sock->async_connect(ep, [sock](const boost::system::error_code &ec) {
-      // If async operation has been cancelled or an error occurred during execution, ec contains corresponding error
-      // code.
-      if (ec != 0) {
-        if (ec == asio::error::operation_aborted) {
-          // If operation cancelled.
-          std::cout << "Operation cancelled";
-        } else {
-          std::cout << "Error occurred! Error code = "
-                    << ec.value()
-                    << ". Message: "
-                    << ec.message();
-        }
-        return;
-      }
-      // At this point the socket is connected and can be used for communication with remote application.
-    });
-
-    // Step 5. Spawn an additional thread and use it to run the Boost.Asio event loop.
-    std::thread worker_thread([&ios]() {
-      try {
-        // Run the io_service object's event processing loop. The run() function blocks until all work has finished and
-        // there are no more handlers to be dispatched, or until the io_service has been stopped.
-        ios.run();
-      } catch (system::system_error &e) {
-        std::cout << "Error occurred! Error code = "
-                  << e.code() << ". Message: "
-                  << e.what();
-      }
-    });
-
-    // Emulating delay.
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    // Step 6. Call the cancel() method on the socket object to cancel all the outstanding asynchronous operations
-    // associated with this socket. If the operation has already finished, calling the cancel() method has no
-    // effect.
-    sock->cancel();
-
-    // Waiting for worker thread to complete.
-    worker_thread.join();
-
+    asio::ip::tcp::socket sock(ios, ep.protocol());
+    sock.connect(ep);
+    // Communication is established.
+    communicate(sock);
   } catch (system::system_error &e) {
     std::cout << "Error occurred! Error code = "
               << e.code() << ". Message: "
@@ -85,5 +37,46 @@ int main() {
     return e.code().value();
   }
 
+  // Stopping server.
+  stop_server(server);
+
   return EXIT_SUCCESS;
+}
+
+void communicate(asio::ip::tcp::socket& sock) {
+  // Allocating and filling the buffer with binary data.
+  const char request_buf[] = {0x48, 0x65, 0x0, 0x6c, 0x6c, 0x6f};
+
+  // Sending the request data.
+  asio::write(sock, asio::buffer(request_buf, sizeof(request_buf)));
+
+  // Shutting down the socket to let the server know that we've sent the whole request.
+  sock.shutdown(asio::socket_base::shutdown_send);
+
+  // We use extensible buffer for response because we don't know the size of the response message.
+  asio::streambuf response_buf;
+
+  system::error_code ec;
+  asio::read(sock, response_buf, ec);
+
+  if (ec == asio::error::eof) {
+    // Whole response message has been received. Here we can handle it. After the client has shut down its socket's send
+    // part, it can still read data from the socket because the receive part of the socket stays open independently from
+    // the send part
+  } else {
+    throw system::system_error(ec);
+  }
+}
+
+std::thread start_server() {
+  return std::thread([]{
+    auto * srv = new server();
+    srv->start();
+  });
+}
+
+void stop_server(std::thread& server) {
+  if (server.joinable()) {
+    server.join();
+  }
 }
---
+++ eduproject/server.hpp
@@ -0,0 +1,59 @@
+//
+// Created by drpsy on 27-Aug-17.
+//
+
+#ifndef EDUPROJECT_SERVER_HPP
+#define EDUPROJECT_SERVER_HPP
+
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace boost;
+
+class server {
+ public:
+  int start() {
+    unsigned short port_num = 3333;
+
+    try {
+      asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(), port_num);
+      asio::io_service ios;
+      asio::ip::tcp::acceptor acceptor(ios, ep);
+      asio::ip::tcp::socket sock(ios);
+      acceptor.accept(sock);
+
+      processRequest(sock);
+    } catch (system::system_error &e) {
+      std::cout << "Error occurred! Error code = " << e.code()
+                << ". Message: " << e.what();
+      return e.code().value();
+    }
+
+    return EXIT_SUCCESS;
+  }
+
+ private:
+  void processRequest(asio::ip::tcp::socket &sock) {
+    // We use extensible buffer because we don't know the size of the request message.
+    asio::streambuf request_buf;
+
+    system::error_code ec;
+
+    // Receiving the request.
+    asio::read(sock, request_buf, ec);
+
+    if (ec != asio::error::eof)
+      throw system::error_code(ec);
+
+    // Request received. Sending response. Allocating and filling the buffer with binary data.
+    const char response_buf[] = {0x48, 0x69, 0x21};
+
+    // Sending the request data.
+    asio::write(sock, asio::buffer(response_buf, sizeof(response_buf)));
+
+    // Shutting down the request to let the client know that we've sent the whole response.
+    sock.shutdown(asio::socket_base::shutdown_send);
+  }
+};
+
+#endif //EDUPROJECT_SERVER_HPP
