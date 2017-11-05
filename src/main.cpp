fbe244e305045f105545d014013999f2e482a7df
Mikhail Gashenko <dr.psycho@live.com>
2017-11-05 19:26:53 +0600

    Getting and setting socket options

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,52 @@
 //
-// Using timers.
+// Getting and setting socket options.
 //
 
-#include <boost/asio/steady_timer.hpp>
+#include <boost/asio.hpp>
 #include <iostream>
 
 using namespace boost;
 
 int main() {
 
-  asio::io_service ios;
+  // Let's consider a hypothetical situation where we want to make the size of the socket's receive buffer two times
+  // bigger than whatever its size now.
 
-  asio::steady_timer t1(ios);
-  // Switches the timer to a non-expired state and starts it.
-  t1.expires_from_now(std::chrono::seconds(2));
+  // Each socket option, whose value can be set or obtained by means of a functionality provided by Boost.Asio, is
+  // represented by a separate class.
 
-  asio::steady_timer t2(ios);
-  t2.expires_from_now(std::chrono::seconds(5));
+  try {
 
-  // Define and set a callback function that is to be called when the t1 expires.
-  t1.async_wait([&t2](boost::system::error_code ec) {
-    if (ec == 0) {
-      std::cout << "Timer #1 has expired!" << std::endl;
-    } else if (ec == asio::error::operation_aborted) {
-      std::cout << "Timer #1 has been cancelled!" << std::endl;
-    } else {
-      std::cout << "Error occurred! Error code = "
-                << ec.value()
-                << ". Message: " << ec.message()
-                << std::endl;
-    }
+    asio::io_service ios;
 
-    t2.cancel();
-  });
+    // Create and open a TCP socket. Before we can get or set options on a particular socket object, the corresponding
+    // socket must be opened.
+    asio::ip::tcp::socket sock(ios, asio::ip::tcp::v4());
 
-  t2.async_wait([](boost::system::error_code ec) {
-    if (ec == 0) {
-      std::cout << "Timer #2 has expired!" << std::endl;
-    } else if (ec == asio::error::operation_aborted) {
-      std::cout << "Timer #2 has been cancelled!" << std::endl;
-    } else {
-      std::cout << "Error occurred! Error code = "
-                << ec.value()
-                << ". Message: " << ec.message()
-                << std::endl;
-    }
-  });
+    // Create an object representing receive buffer size option.
+    asio::socket_base::receive_buffer_size cur_buf_size;
 
-  // The method blocks until both the timers expire. The expiration callbacks will be called in the context of
-  // the thread that called 'run()'.
-  ios.run();
+    // Get the currently set value of the option. Method deduces the option that is requested by the type of the
+    // argument passed to it.
+    sock.get_option(cur_buf_size);
+
+    std::cout << "Current receive buffer size is " << cur_buf_size.value() << " bytes." << std::endl;
+
+    // Create an object representing receive buffer size option with new value.
+    asio::socket_base::receive_buffer_size new_buf_size(cur_buf_size.value() * 2);
+
+    // Set new value of the option.
+    sock.set_option(new_buf_size);
+
+    std::cout << "New receive buffer size is "
+              << new_buf_size.value() << " bytes." << std::endl;
+
+  } catch (system::system_error &e) {
+    std::cout << "Error occurred! Error code = "
+              << e.code() << ". Message: " << e.what();
+
+    return e.code().value();
+  }
 
   return EXIT_SUCCESS;
 }
