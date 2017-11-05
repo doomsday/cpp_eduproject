9b46668e56ab32f717ea4397efd7cf9d1e87d888
Mikhail Gashenko <dr.psycho@live.com>
2017-11-05 16:44:31 +0600

    Using composite buffers for scatter/gather operations: Preparing a composite buffer for an input operation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 //
-// Using composite buffers for scatter/gather operations: Preparing a composite buffer for gather output operations.
+// Using composite buffers for scatter/gather operations: Preparing a composite buffer for an input operation.
 //
 
 #include <boost/asio.hpp>
@@ -7,21 +7,22 @@
 using namespace boost;
 
 int main() {
-  // Steps 1 and 2. Create and fill simple buffers.
-  const char *part1 = "Hello ";
-  const char *part2 = "my ";
-  const char *part3 = "friend!";
 
-  // Step 3. Create an object representing a composite buffer.
-  std::vector<asio::const_buffer> composite_buffer;
+  // Step 1. Allocate simple buffers.
+  char part1[6];
+  char part2[3];
+  char part3[7];
 
-  // Step 4. Add simple buffers to the composite buffer.
-  composite_buffer.push_back(asio::const_buffer(part1, 6));
-  composite_buffer.push_back(asio::const_buffer(part2, 3));
-  composite_buffer.push_back(asio::const_buffer(part3, 7));
+  // Step 2. Create an object representing a composite buffer.
+  std::vector<asio::mutable_buffer> composite_buffer;
 
-  // Step 5. Now composite_buffer can be used with Boost.Asio output operations as if it was
-  // as simple buffer represented by contiguous block of memory.
+  // Step 3. Add simple buffers to the composite buffer object.
+  composite_buffer.push_back(asio::mutable_buffer(part1, sizeof(part1)));
+  composite_buffer.push_back(asio::mutable_buffer(part2, sizeof(part2)));
+  composite_buffer.push_back(asio::mutable_buffer(part3, sizeof(part3)));
+
+  // Now composite_buffer can be used with Boost.Asio input operation as if it was a simple buffer
+  // represented by contiguous block of memory.
 
   return EXIT_SUCCESS;
 }
