004ccbc24c3c73e8e643948a02e8fefe5db24add
Mikhail Gashenko <dr.psycho@live.com>
2017-11-05 19:47:55 +0600

    Performing a stream-based I/O

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,38 @@
 //
-// Getting and setting socket options.
+// Performing a stream-based I/O.
+//
+// Boost.Asio provides tools that allow us to implement inter-process communication in a stream-based fashion.
+// In this recipe, we will see how to use them.
 //
 
 #include <boost/asio.hpp>
 #include <iostream>
+#include <chrono>
 
 using namespace boost;
 
 int main() {
 
-  // Let's consider a hypothetical situation where we want to make the size of the socket's receive buffer two times
-  // bigger than whatever its size now.
+  // Wraps a TCP socket and provides an I/O stream-like interface to it. Automatically tries to resolve the DNS name
+  // and then tries to connect to that server.
+  asio::ip::tcp::iostream stream("google.com", "http");  // Or ("google.com", "http").
 
-  // Each socket option, whose value can be set or obtained by means of a functionality provided by Boost.Asio, is
-  // represented by a separate class.
-
-  try {
-
-    asio::io_service ios;
-
-    // Create and open a TCP socket. Before we can get or set options on a particular socket object, the corresponding
-    // socket must be opened.
-    asio::ip::tcp::socket sock(ios, asio::ip::tcp::v4());
-
-    // Create an object representing receive buffer size option.
-    asio::socket_base::receive_buffer_size cur_buf_size;
-
-    // Get the currently set value of the option. Method deduces the option that is requested by the type of the
-    // argument passed to it.
-    sock.get_option(cur_buf_size);
-
-    std::cout << "Current receive buffer size is " << cur_buf_size.value() << " bytes." << std::endl;
-
-    // Create an object representing receive buffer size option with new value.
-    asio::socket_base::receive_buffer_size new_buf_size(cur_buf_size.value() * 2);
-
-    // Set new value of the option.
-    sock.set_option(new_buf_size);
-
-    std::cout << "New receive buffer size is "
-              << new_buf_size.value() << " bytes." << std::endl;
-
-  } catch (system::system_error &e) {
+  // Test whether connection has succeeded.
+  if (!stream) {
     std::cout << "Error occurred! Error code = "
-              << e.code() << ". Message: " << e.what();
-
-    return e.code().value();
+              << stream.error().value()
+              << ". Message = " << stream.error().message()
+                                << std::endl;
+    return EXIT_FAILURE;
   }
+
+  stream << "Request.";
+  stream.flush(); // Make sure all the buffered data is pushed to the server.
+
+  stream.expires_from_now(boost::posix_time::seconds(10));
+
+  std::cout << "Response: "
+            << stream.rdbuf();  // Return a pointer to the underlying streambuf.
 
   return EXIT_SUCCESS;
 }
